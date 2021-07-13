b1b3d51f59aca4c6f410e2746f5115d743a26987
myt10 <myt10@126.com>
2021-07-13 13:58:49 +0800

    20210713
    补全interpretml中的解释算法

--- InterpretML/transform.py
+++ InterpretML/transform.py
@@ -1,39 +1,19 @@
 # -*- coding: utf-8 -*-
-from interpret.blackbox import LimeTabular
+
 import pandas as pd
 import numpy as np
 
-class lime_transformed(LimeTabular):
-    def __init__(self,
-        predict_fn,
-        data,
-        sampler=None,
-        feature_names=None,
-        feature_types=None,
-        explain_kwargs={},
-        n_jobs=1,
-        **kwargs):
-        LimeTabular.__init__(self,
-        predict_fn,
-        data,
-        sampler=None,
-        feature_names=None,
-        feature_types=None,
-        explain_kwargs={},
-        n_jobs=1,
-        **kwargs)
-    def init_transform(self,file_path):
-        with open(file_path,"r",encoding='utf-8') as f:
-            name_list = f.readlines()
-        raw_list = []
-        for i in range(len(name_list)):
-            raw_list.append(name_list[i].split(":")[0])
-            name_list[i] = name_list[i].split(":")[1][:-1]
-        cnt = 0
-        for name in name_list:
-            if name != '':
-                #print("name: ",name)
-                #print(self.feature_names.index(raw_list[cnt]))
-                self.feature_names[self.feature_names.index(raw_list[cnt])] = name
-            cnt += 1
-        #print(self.feature_names)
+def init_transform(df,file_path):
+    with open(file_path,"r",encoding='utf-8') as f:
+        name_list = f.readlines()
+    raw_list = []
+    for i in range(len(name_list)):
+        raw_list.append(name_list[i].split(":")[0])
+        name_list[i] = name_list[i].split(":")[1][:-1]
+    cnt = 0
+    for name in name_list:
+        if name != '':
+            print("name: ",name)
+            print(raw_list[cnt])
+            df.rename(columns={raw_list[cnt]:name},inplace=True)
+        cnt += 1
